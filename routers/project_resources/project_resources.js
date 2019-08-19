46d3fdc436287796ac5253a7e0dcfe0782be3149
Eric Hendrix <agentmrbig@gmail.com>
2019-08-18 23:15:54 -0400

    got mvp, tried to do some of the stretch, my knex migrate:rollback and knex seed:run dont work, tried many different tutorials and documentation

--- routers/project_resources/project_resources.js
+++ routers/project_resources/project_resources.js
@@ -3,28 +3,29 @@ const db = require('../../data/dbConfig')
 
 router.get('/', async (req, res) => {
     try {
-        const resources = await db('resources')
+        const resources = await db('project_resources')
         res.json(resources)
     } catch (e) {
         res.status(500).json({
-            error: 'Error, could not get resources', e
+            error: 'Error, could not get project resources', e
         })
     }
 })
 
 router.post('/', async (req, res) => {
-    const { name, description } = req.body
-
-    if (!name) {
+    const { project_id, resource_id } = req.body
+    if (!project_id || !resource_id) {
+        console.log(req.body)
         return res.status(400).json({
-            error: 'Name property is required!'
+            error: 'project_id and resource_id properties are required!'
         })
     }
     try {
-        const [id] = await db('resources').insert({ name, description })
-        const [resource] = await db('resources').where({ id })
+        const [id] = await db('project_resources').insert({ project_id, resource_id })
+        const [resource] = await db('project_resources').where({ id })
         res.status(201).json(resource)
     } catch (e) {
+        console.log(req.body)
         res.status(500).json({
             error: 'Error, could not add resource', e
         })
--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const projectsRouter = require('./routers/projects/projects');
 const resourcesRouter = require('./routers/resources/resources');
+const tasksRouter = require('./routers/tasks/tasks');
+const projectResourcesRouter = require('./routers/project_resources/project_resources');
 
 const server = express();
 
@@ -8,5 +10,7 @@ server.use(express.json());
 
 server.use('/api/projects', projectsRouter);
 server.use('/api/resources', resourcesRouter);
+server.use('/api/tasks', tasksRouter);
+server.use('/api/project-resources', projectResourcesRouter);
 
 module.exports = server;
