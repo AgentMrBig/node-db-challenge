a169008bd220249bea2ee5909ac718f832dda995
Eric Hendrix <agentmrbig@gmail.com>
2019-08-18 22:19:45 -0400

    added endpoint for resources

--- routers/projects/projects.js
+++ routers/projects/projects.js
@@ -35,4 +35,31 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        let [project] = await db('projects').where({ id })
+
+        if (!project) {
+            return res.status(404).json({
+                error: `Project with id: ${id}, does not exist`
+            })
+        } else {
+            const tasks = await db('tasks').where({ project_id: id })
+            const resources = await db('resources')
+                .whereIn('id', function () {
+                    this.select('resource_id')
+                        .from('project_resources')
+                        .where({ project_id: id })
+                })
+            project = { ...project, tasks, resources }
+            return res.json(project)
+        }
+    } catch (e) {
+        res.status(500).json({
+            error: 'Error, could not get project!', e
+        })
+    }
+})
+
 module.exports = router
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const projectsRouter = require('./routers/projects/projects');
+const resourcesRouter = require('./routers/resources/resources');
 
 const server = express();
 
 server.use(express.json());
 
 server.use('/api/projects', projectsRouter);
+server.use('/api/resources', resourcesRouter);
 
 module.exports = server;
